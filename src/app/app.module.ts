434b06e9ef4a299e3f3f23e2a4a01423ffcc76af
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-03-25 08:58:59 +0300

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PromoCarouselModule } from './shared/modules/promo-carousel/promo-carousel.module';
+import { TopBarModule } from './shared/modules/top-bar/top-bar.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TopBarModule,
+        PromoCarouselModule,
+        NgbModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
