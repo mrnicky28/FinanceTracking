146ea4b0d201790dd22df21e5d6a6f60aee81a80
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-03-25 15:56:05 +0300

    add promo carousel component

--- src/app/shared/modules/promo-carousel/components/promo-carousel/promo-carousel.component.ts
+++ src/app/shared/modules/promo-carousel/components/promo-carousel/promo-carousel.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
-import { PromoCarouselOptions } from '../../interfaces/promo-carousel-options.interfaces';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-promo-carousel',
     templateUrl: './promo-carousel.component.html',
     styleUrls: ['./promo-carousel.component.scss'],
 })
-export class PromoCarouselComponent {}
+export class PromoCarouselComponent {
+    showNavigationArrows = false;
+    showNavigationIndicators = false;
+    images = [944, 1011, 984].map(
+        (n) => `https://picsum.photos/id/${n}/900/500`,
+    );
+    constructor(config: NgbCarouselConfig) {
+        // customize default values of carousels used by this component tree
+        config.showNavigationArrows = true;
+        config.showNavigationIndicators = true;
+    }
+}
--- src/app/shared/modules/promo-carousel/promo-carousel.module.ts
+++ src/app/shared/modules/promo-carousel/promo-carousel.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { PromoCarouselComponent } from './components/promo-carousel/promo-carousel.component';
 
 @NgModule({
     declarations: [PromoCarouselComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, NgbModule],
     exports: [PromoCarouselComponent],
 })
 export class PromoCarouselModule {}
