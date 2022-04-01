fa94fafb4d86e2c28b885125086b0ec7e12f18a2
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-01 10:31:10 +0300

    add tob-bar component and module

--- src/app/shared/modules/top-bar/components/top-bar/top-bar.component.ts
+++ src/app/shared/modules/top-bar/components/top-bar/top-bar.component.ts
@@ -1,15 +1,29 @@
+import { LoginComponent } from 'src/app/auth/components/login/login/login.component';
+import { RegisterComponent } from 'src/app/auth/components/register/register/register.component';
+
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.scss']
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.scss'],
 })
 export class TopBarComponent implements OnInit {
+    constructor(public dialog: MatDialog, private router: Router) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    signIn(): void {
+        const dialogConfig = new MatDialogConfig();
+        this.dialog.open(LoginComponent);
+        this.router.navigate(['/login']);
+    }
+    signUp(): void {
+        const dialogRef = this.dialog.open(RegisterComponent, {
+            width: '0px',
+        });
+        this.router.navigate(['/register']);
+    }
 }
--- src/app/shared/modules/top-bar/top-bar.module.ts
+++ src/app/shared/modules/top-bar/top-bar.module.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { CurrencyTickerModule } from '../currency-ticker/currency-ticker.module';
 import { PromoCarouselModule } from '../promo-carousel/promo-carousel.module';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
     declarations: [TopBarComponent],
-    imports: [CommonModule, PromoCarouselModule],
+    imports: [
+        CommonModule,
+        RouterModule,
+        PromoCarouselModule,
+        CurrencyTickerModule,
+        NgbModule,
+    ],
     exports: [TopBarComponent],
 })
 export class TopBarModule {}
