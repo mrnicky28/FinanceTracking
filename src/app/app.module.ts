2d11851f3ac669013ba92931e432f875df3b6f67
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-01 11:28:49 +0300

    add app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { CurrencyTickerModule } from './shared/modules/currency-ticker/currency-ticker.module';
+import { MaterialModule } from './shared/modules/material/material.module';
 import { PromoCarouselModule } from './shared/modules/promo-carousel/promo-carousel.module';
 import { TopBarModule } from './shared/modules/top-bar/top-bar.module';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
-        BrowserModule,
+        MaterialModule,
+        MatDialogModule,
+        AuthModule,
         AppRoutingModule,
+        BrowserModule,
         TopBarModule,
         PromoCarouselModule,
         NgbModule,
+        CurrencyTickerModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
