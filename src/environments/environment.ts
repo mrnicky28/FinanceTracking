29359f62cf65ad5875b62f3d679e6146825b26c8
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-01 10:31:38 +0300

    add firebase enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'finance-tracking-app-3fabe',
+    appId: '1:696256391940:web:0b3aca63e27e7f80fd3e51',
+    storageBucket: 'finance-tracking-app-3fabe.appspot.com',
+    apiKey: 'AIzaSyDctTTyygJR9GHcvuC1iL97L41ox-7J7h4',
+    authDomain: 'finance-tracking-app-3fabe.firebaseapp.com',
+    messagingSenderId: '696256391940',
+  },
   production: false
 };
 
