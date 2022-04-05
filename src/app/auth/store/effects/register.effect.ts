6a8b4aff2f7415fc0cb6c5efa29d9411a4bd5f04
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-05 09:47:49 +0300

    added register effect

---
+++ src/app/auth/store/effects/register.effect.ts
@@ -0,0 +1,33 @@
+import { of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.interface';
+
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+
+import { AuthService } from '../../services/authentication/auth.service';
+import {
+    registerAction, registerFailureAction, registerSuccessAction
+} from '../actions/register.action';
+
+@Injectable()
+export class RegisterEffect {
+    constructor(private actions$: Actions, private authService: AuthService) {}
+
+    register$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(registerAction),
+            switchMap(({ request }) => {
+                return this.authService.register(request).pipe(
+                    map((currentUser: CurrentUserInterface) => {
+                        return registerSuccessAction({ currentUser });
+                    }),
+                    catchError((errorResponse: HttpErrorResponse) => {
+                        return of(registerFailureAction());
+                    }),
+                );
+            }),
+        ),
+    );
+}
