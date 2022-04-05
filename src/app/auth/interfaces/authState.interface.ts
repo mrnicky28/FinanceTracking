602e98c301d3c86fd0957314628a74921b3bfba4
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-05 19:12:08 +0300

    update interface

--- src/app/auth/interfaces/authState.interface.ts
+++ src/app/auth/interfaces/authState.interface.ts
@@ -1,9 +1,8 @@
-import { BackendErrorsInterface } from 'src/app/shared/interfaces/backendErrors.interface';
 import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.interface';
 
 export interface AuthStateInterface {
     isSubmitting: boolean;
     currentUser: CurrentUserInterface | null;
     isLoggedIn: boolean | null;
-    validationErrors: BackendErrorsInterface | null;
+    validationErrors: string[] | null;
 }
