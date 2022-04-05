d7ca696884450a8ac1512d952c2491e5f2de99ff
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-05 09:43:57 +0300

    added method onSubmit and remove redirect

--- src/app/auth/components/register/register/register.component.ts
+++ src/app/auth/components/register/register/register.component.ts
@@ -1,7 +1,14 @@
+import { Observable } from 'rxjs';
+import { RegisterRequestInterface } from 'src/app/auth/interfaces/registerRequest.interface';
+import { AuthService } from 'src/app/auth/services/authentication/auth.service';
+import { registerAction } from 'src/app/auth/store/actions/register.action';
+import { isSubmittingSelector } from 'src/app/auth/store/selector';
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { select, Store } from '@ngrx/store';
 
 import { LoginComponent } from '../../login/login/login.component';
 
@@ -11,37 +18,54 @@ import { LoginComponent } from '../../login/login/login.component';
     styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-    form!: FormGroup;
+    registerForm!: FormGroup;
+    isSubmitting$!: Observable<boolean>;
 
     constructor(
         private fb: FormBuilder,
+        private store: Store,
         private dialog: MatDialog,
         private router: Router,
+        private authService: AuthService,
     ) {}
 
     ngOnInit(): void {
         this.initializeForm();
+        this.initializeValues();
+    }
+
+    initializeValues(): void {
+        this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+        console.log('isSubmitting', this.isSubmitting$);
     }
 
     initializeForm(): void {
-        this.form = this.fb.group({
-            username: ['', Validators.required],
+        this.registerForm = this.fb.group({
+            username: ['', [Validators.required, Validators.maxLength(32)]],
             email: ['', Validators.required],
-            password: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(10)]],
+            // confirPassword: ['', Validators.required],
         });
     }
 
     onSubmit(): void {
-        console.log(this.form.value);
+        if (this.registerForm.invalid) return;
+
+        const request: RegisterRequestInterface = {
+            ...this.registerForm.value,
+        };
+        this.store.dispatch(registerAction({ request }));
+        this.registerForm.reset();
     }
 
     onClose(): void {
         this.dialog.closeAll();
-        this.router.navigate(['']);
+        // this.router.navigate(['']);
     }
+
     redirectToLogin(): void {
         this.dialog.closeAll();
-        this.router.navigate(['/login']);
+        // this.router.navigate(['/login']);
         this.dialog.open(LoginComponent);
     }
 }
