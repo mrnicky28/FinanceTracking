67b2afb5d664a42ee9109e1ea7c388e7708a062d
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-05 12:23:33 +0300

    added register success and failure reducers

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,10 +1,16 @@
 import { Action, createReducer, on } from '@ngrx/store';
-
 import { AuthStateInterface } from '../interfaces/authState.interface';
-import { registerAction } from './actions/register.action';
+import {
+    registerAction,
+    registerFailureAction,
+    registerSuccessAction,
+} from './actions/register.action';
 
 const initialState: AuthStateInterface = {
     isSubmitting: false,
+    currentUser: null,
+    isLoggedIn: null,
+    validationErrors: null,
 };
 
 const authReducer = createReducer(
@@ -14,6 +20,25 @@ const authReducer = createReducer(
         (state): AuthStateInterface => ({
             ...state,
             isSubmitting: true,
+            validationErrors: null,
+        }),
+    ),
+    on(
+        registerSuccessAction,
+        (state, action): AuthStateInterface => ({
+            ...state,
+            isSubmitting: false,
+            isLoggedIn: true,
+            currentUser: action.currentUser,
+            validationErrors: null,
+        }),
+    ),
+    on(
+        registerFailureAction,
+        (state, action): AuthStateInterface => ({
+            ...state,
+            isSubmitting: false,
+            validationErrors: action.errors,
         }),
     ),
 );
