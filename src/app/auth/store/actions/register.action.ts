c5765a10194e650b5734dc108fb3522581899f1e
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-05 09:47:36 +0300

    added register action

---
+++ src/app/auth/store/actions/register.action.ts
@@ -0,0 +1,18 @@
+import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.interface';
+
+import { createAction, props } from '@ngrx/store';
+
+import { RegisterRequestInterface } from '../../interfaces/registerRequest.interface';
+import { ActionTypes } from '../actionType';
+
+export const registerAction = createAction(
+    ActionTypes.REGISTER,
+    props<{ request: RegisterRequestInterface }>(),
+);
+
+export const registerSuccessAction = createAction(
+    ActionTypes.REGISTER_SUCCESS,
+    props<{ currentUser: CurrentUserInterface }>(),
+);
+
+export const registerFailureAction = createAction(ActionTypes.REGISTER_FAILURE);
