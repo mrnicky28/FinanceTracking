aac3215b59735bb841f7ce36eff1b77f495667c3
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-07 10:46:00 +0300

    added matspinner in material module

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-    exports: [MatDialogModule, MatFormFieldModule],
+    exports: [MatDialogModule, MatFormFieldModule, MatProgressSpinnerModule],
 })
 export class MaterialModule {}
