109642fca3b424ed51d56f0443a934ac7eeb83ca
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-07 14:18:55 +0300

    getting a token in localStorage and added regirect

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,21 +1,28 @@
+import { of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.interface';
+import { PersistanceService } from 'src/app/shared/services/persistance/persistance.service';
+
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.interface';
+
 import { AuthService } from '../../services/authentication/auth.service';
 import {
-    registerAction,
-    registerFailureAction,
-    registerSuccessAction,
+    registerAction, registerFailureAction, registerSuccessAction
 } from '../actions/register.action';
-
-
 
 @Injectable()
 export class RegisterEffect {
-    constructor(private actions$: Actions, private authService: AuthService) {}
+    constructor(
+        private actions$: Actions,
+        private authService: AuthService,
+        private persistanceService: PersistanceService,
+        private router: Router,
+        public dialog: MatDialog,
+    ) {}
 
     register$ = createEffect(() =>
         this.actions$.pipe(
@@ -23,15 +30,17 @@ export class RegisterEffect {
             switchMap(({ request }) => {
                 return this.authService.register(request).pipe(
                     map((currentUser: CurrentUserInterface) => {
+                        this.persistanceService.set(
+                            'accessToken',
+                            currentUser.user.accessToken,
+                        );
                         return registerSuccessAction({ currentUser });
                     }),
                     catchError((errorResponse: HttpErrorResponse) => {
                         return of(
                             registerFailureAction({
                                 error: [
-                                    errorResponse.message
-                                        .split('(')[1]
-                                        .split(')')[0],
+                                    errorResponse.message.split('(')[1].split(')')[0],
                                 ],
                             }),
                         );
@@ -39,5 +48,17 @@ export class RegisterEffect {
                 );
             }),
         ),
+    );
+
+    redirectAfterSubmit$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(registerSuccessAction),
+                tap(() => {
+                    this.router.navigate(['/']);
+                    this.dialog.closeAll();
+                }),
+            ),
+        { dispatch: false },
     );
 }
