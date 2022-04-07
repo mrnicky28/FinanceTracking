9cc743f4bb248cd790db5b0e4650b70b75afbf28
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-07 20:17:46 +0300

    added login effect

---
+++ src/app/auth/store/effects/login.effect.ts
@@ -0,0 +1,63 @@
+import { of } from 'rxjs';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { catchError, map, tap } from 'rxjs/operators';
+import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.interface';
+import { PersistanceService } from 'src/app/shared/services/persistance/persistance.service';
+
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+
+import { AuthService } from '../../services/authentication/auth.service';
+import { loginAction, loginFailureAction, loginSuccessAction } from '../actions/login.action';
+
+@Injectable()
+export class LoginEffect {
+    constructor(
+        private router: Router,
+        private dialog: MatDialog,
+        private actions$: Actions,
+        private authService: AuthService,
+        private persistanceService: PersistanceService,
+    ) {}
+
+    login$ = createEffect(() =>
+        this.actions$.pipe(
+            ofType(loginAction),
+            switchMap(({ request }) => {
+                return this.authService.login(request).pipe(
+                    map((currentUser: CurrentUserInterface) => {
+                        this.persistanceService.set(
+                            'accessToken',
+                            currentUser.user.accessToken,
+                        );
+                        return loginSuccessAction({ currentUser });
+                    }),
+                    catchError((errorResponse: HttpErrorResponse) => {
+                        return of(
+                            loginFailureAction({
+                                error: [
+                                    errorResponse.message.split('(')[1].split(')')[0],
+                                ],
+                            }),
+                        );
+                    }),
+                );
+            }),
+        ),
+    );
+
+    redirectAfterSubmit$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(loginSuccessAction),
+                tap(() => {
+                    this.router.navigate(['/']);
+                    this.dialog.closeAll();
+                }),
+            ),
+        { dispatch: false },
+    );
+}
