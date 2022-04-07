f2cb75d3f8db6c736bc8a9d43e5bba1e995e64f8
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-07 10:59:05 +0300

    adding a loading flag to the interface

--- src/app/auth/interfaces/authState.interface.ts
+++ src/app/auth/interfaces/authState.interface.ts
@@ -2,6 +2,7 @@ import { CurrentUserInterface } from 'src/app/shared/interfaces/currentUser.inte
 
 export interface AuthStateInterface {
     isSubmitting: boolean;
+    isLoading: boolean;
     currentUser: CurrentUserInterface | null;
     isLoggedIn: boolean | null;
     validationErrors: string[] | null;
