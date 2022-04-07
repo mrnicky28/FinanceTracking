fb6ce9509d47e85a521bd679b7422349da5b99e6
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-07 10:51:12 +0300

    added isLoadingSelector

--- src/app/auth/store/selector.ts
+++ src/app/auth/store/selector.ts
@@ -9,6 +9,12 @@ export const isSubmittingSelector = createSelector(
     authFeatureSelector,
     (authState: AuthStateInterface) => authState.isSubmitting,
 );
+
+export const isLoadingSelector = createSelector(
+    authFeatureSelector,
+    (authState: AuthStateInterface) => authState.isLoading
+);
+
 export const validationErrorsSelector = createSelector(
     authFeatureSelector,
     (authState: AuthStateInterface) => authState.validationErrors,
