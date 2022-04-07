894feaacbc32dfb0117408b6756321d310d7f20b
mrnicky28 <lavrinovich_nikita@mail.ru>
2022-04-07 14:19:53 +0300

    deleted console.log and rename variable

--- src/app/auth/validators/matchPassword.validator.ts
+++ src/app/auth/validators/matchPassword.validator.ts
@@ -1,17 +1,11 @@
 import { FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export class MatchPasswordValidator {
-    static matchPassword(
-        psswordControl: string,
-        confirmPasswordControl: string,
-    ): ValidatorFn | any {
+    static matchPassword(passwordControl: string, confirmPasswordControl: string): ValidatorFn | any {
         return (formGroup: FormGroup): ValidationErrors | null => {
-            const password = formGroup.get(psswordControl)?.value;
-            const confirmPassword = formGroup.get(
-                confirmPasswordControl,
-            )?.value;
+            const password = formGroup.get(passwordControl)?.value;
+            const confirmPassword = formGroup.get(confirmPasswordControl)?.value;
 
-            console.log(password, confirmPassword, password == confirmPassword);
             if (password == confirmPassword) {
                 return null;
             }
